3db9fe6224bc8713c2dc59f14528673194915e77
Liz Ing-Simmons <liz.ing-simmons@gmail.com>
2015-03-10 18:22:52 +0000

    added tests for validity

--- tests/testthat/test_getters.R
+++ tests/testthat/test_getters.R
@@ -1,13 +1,13 @@
 make_gi <- function() {
   new("GenomicInteractions",
       metadata = list(experiment_name="test", description = "this is a test"),
-      anchor_one = GRanges(seqnames = Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+      anchor_one = GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
                            ranges = IRanges(1:10, width = 10:1),
-                           strand = Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                           strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
                            seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep=""))),
-      anchor_two = GRanges(seqnames = Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+      anchor_two = GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
                            ranges = IRanges(101:110, width = 10:1),
-                           strand = Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                           strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
                            seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep=""))),
       counts = as.integer(10:1),
       elementMetadata = new("DataFrame", nrows = as.integer(10))
@@ -16,13 +16,13 @@ make_gi <- function() {
 
 test_that("Anchors are returned correctly", {
   gi <- make_gi()
-  expect_anchor_one <- GRanges(seqnames = Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+  expect_anchor_one <- GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
                                ranges = IRanges(1:10, width = 10:1),
-                               strand = Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                               strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
                                seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep="")))
-  expect_anchor_two <- GRanges(seqnames = Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+  expect_anchor_two <- GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
                                ranges = IRanges(101:110, width = 10:1),
-                               strand = Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                               strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
                                seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep="")))
   
   expect_equal(anchorOne(gi), expect_anchor_one)
@@ -53,14 +53,14 @@ test_that("Annotation features are returned correctly", {
   
   gi <- new("GenomicInteractions",
             metadata = list(experiment_name="test", description = "this is a test"),
-            anchor_one = GRanges(seqnames = Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+            anchor_one = GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
                                  ranges = IRanges(1:10, width = 10:1),
-                                 strand = Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                                 strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
                                  seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep="")),
                                  node.class = rep(c("distal", "distal", "promoter", "promoter", "promoter"),2)),
-            anchor_two = GRanges(seqnames = Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+            anchor_two = GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
                                  ranges = IRanges(101:110, width = 10:1),
-                                 strand = Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                                 strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
                                  seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep="")),
                                  node.class = rep(c("distal", "distal", "promoter", "distal", "promoter"), 2)),
             counts = as.integer(10:1),
