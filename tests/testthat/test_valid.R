aa168fc2d3087a532d4532d5aed5e7592b76c6aa
Liz Ing-Simmons <liz.ing-simmons@gmail.com>
2015-03-11 13:14:04 +0000

    tests for annotation and seqinfo

--- tests/testthat/test_valid.R
+++ tests/testthat/test_valid.R
@@ -75,12 +75,30 @@ make_bad_counts <- function() {
 }
 
 
+make_neg_counts <- function() {
+  new("GenomicInteractions",
+      metadata = list(experiment_name="test", description = "this is a test"),
+      anchor_one = GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+                           ranges = IRanges(1:10, width = 10:1),
+                           strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                           seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep=""))),
+      anchor_two = GRanges(seqnames = S4Vectors::Rle(factor(c("chr1", "chr2", "chr1", "chr3")), c(1, 3, 2, 4)),
+                           ranges = IRanges(101:110, width = 10:1),
+                           strand = S4Vectors::Rle(strand(c("-", "+", "+", "-", "-", "-")), c(1, 2, 1, 1, 3, 2)),
+                           seqinfo = Seqinfo(seqnames = paste("chr", 1:3, sep=""))),
+      counts = as.integer(8:-1),
+      elementMetadata = new("DataFrame", nrows = as.integer(10))
+  )
+}
+
+
 
 
 test_that("Invalid objects are invalid", {
   expect_error(make_no_anchors(), "anchor one cannot be of length 0")
   expect_error(make_unequal_anchors(), "length of anchor one and anchor two do not match")
-  expect_eror(make_diff_seqinfo(), "seqinfo must be indentical for both GRanges")
-  
+  expect_error(make_diff_seqinfo(), "seqinfo must be indentical for both GRanges")
+  expect_error(make_bad_counts(), "length of counts must be the same as the anchors")
+  expect_error(make_neg_counts(), "Counts should contain only non-negative integers")
   
 })
