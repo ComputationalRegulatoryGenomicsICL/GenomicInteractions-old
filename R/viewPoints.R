83a95d5faee2b2fbfe5ee05c9b56d1b461dc7029
Liz Ing-Simmons <liz.ingsimmons@gmail.com>
2015-05-08 19:01:31 +0100

    fixed viewpoints bug

--- R/viewPoints.R
+++ R/viewPoints.R
@@ -54,7 +54,7 @@ plotViewpoint = function(x, region, ...) {
     x = x[overlapsAny(x@anchor_two, region, type="within")]
     cov = as(coverage(x@anchor_two)[region], "GRanges")
     points_x = c(start(cov), end(cov)) + start(region)
-    points_y = rep.int(x$score, 2)
+    points_y = rep.int(cov$score, 2)
     ord = order(points_x)
     p = plot(points_x[ord], points_y[ord], t="l", ...)
     return(p)
