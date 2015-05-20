de25ed55ee6394162ebd3c9cac3fdca537fed5c1
nathan <iwanttobeabadger@gmail.com>
2015-05-20 15:52:46 +0800

    added availableDisplayPars and options to colour anchors in using node.class and control the colour of their line and fill independently

--- R/InteractionTrack-class.R
+++ R/InteractionTrack-class.R
@@ -4,7 +4,7 @@
 #'
 #'  @slot plottingFunction function
 #'  @slot variables list
-#'  @slot chromosome
+#'  @slot chromosome chromosome
 #'  @slot stacking character
 #'
 #' InteractionTrack is a specific Gviz-derived class for enabling the visualisation of chromatin interaction data. 
@@ -36,9 +36,12 @@ setClass("InteractionTrack",
                                        chromosome="character",
                                        stacking="character"),
          prototype=prototype(dp=DisplayPars(plot.anchors=TRUE,
-                                            col.anchors = "lightblue",
-                                            interaction.strength="width", 
-                                            lwd.interaction ="counts",
+                                            col.anchors.line = "lightblue",
+                                            col.anchors.fill = "lightblue",
+                                            col.anchors.line.node.class = c(),
+                                            col.anchors.fill.node.class = c(),
+                                            interaction.strength="width", # TODO
+                                            lwd.interaction ="counts", # TODO
                                             col.interactions = "red",
                                             plot.outside = TRUE,
                                             col.outside = "red",
@@ -50,7 +53,7 @@ setClass("InteractionTrack",
 
 
 setMethod("initialize", "InteractionTrack", function(.Object, giobject, chromosome, variables, ...) {
-  #.Object <- .updatePars(.Object, "InteractionTrack") #
+  .Object <- Gviz:::.updatePars(.Object, "InteractionTrack") #
   .Object@giobject <- giobject
   .Object@chromosome <- chromosome
   .Object@variables <- variables
@@ -113,7 +116,7 @@ setMethod("subset", signature(x="InteractionTrack"), function(x, from, to, chrom
 #' plotTracks(list(interactions.track), chromosome="chr1", from=0, to=500)
 #' 
 #' @export
-InteractionTrack <- function(x, chromosome="", name=NULL, start=NULL, end=NULL){ # TODO SORT OUT START AND STOP HERE
+InteractionTrack <- function(x, chromosome="", name=NULL, start=NULL, end=NULL){ 
   if(!(class(x)=="GenomicInteractions")){ stop("x must be a GenomicInteractions object")}
   if(is.null(name)){
     name = name(x)
@@ -152,7 +155,7 @@ setMethod("drawGD", signature("InteractionTrack"), function(GdObject, minBase, m
   }
   
   if(prepare){
-      pushViewport(viewport(xscale=c(minBase, maxBase), yscale=c(0, 1) )) # TESTING
+      pushViewport(viewport(xscale=c(minBase, maxBase), yscale=c(0, 1) )) 
       popViewport(1)
       return(invisible(GdObject))
     }
@@ -160,8 +163,11 @@ setMethod("drawGD", signature("InteractionTrack"), function(GdObject, minBase, m
   pushViewport(viewport(xscale=c(minBase, maxBase), yscale=c(0, 1)))
   if(length(GdObject@giobject)>0){
     plot.anchors = displayPars(GdObject, "plot.anchors")
-    col.anchors = displayPars(GdObject, "col.anchors")
+    col.anchors.line = displayPars(GdObject, "col.anchors.line")
+    col.anchors.fill = displayPars(GdObject, "col.anchors.fill")
+    col.anchors.fill.node.class = displayPars(GdObject, "col.anchors.fill.node.class")
     anchor.height = displayPars(GdObject, "anchor.height")
+  
     col.interactions = displayPars(GdObject, "col.interactions")
     plot.outside = displayPars(GdObject, "plot.outside")
     col.outside =  displayPars(GdObject, "col.outside")
@@ -217,15 +223,19 @@ setMethod("drawGD", signature("InteractionTrack"), function(GdObject, minBase, m
       for(i in trans.indexes){
         if( anchor_one_chr[i] != GdObject@chromosome ){
           if(anchor_two_midpoints[i] > ((minBase + maxBase)/2)){
-            grid.curve(x1=anchor_two_midpoints[i], y1=y.start, x2=maxBase, y2=0.95,  curvature = -1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
+            grid.curve(x1=anchor_two_midpoints[i], y1=y.start, x2=maxBase, y2=0.95,  
+                       curvature = -1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
           }else{
-            grid.curve(x1=anchor_two_midpoints[i], y1=y.start, x2=minBase, y2=0.95,  curvature = 1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
+            grid.curve(x1=anchor_two_midpoints[i], y1=y.start, x2=minBase, y2=0.95,  
+                       curvature = 1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
           }      
         }else if( anchor_two_chr[i] != GdObject@chromosome){
           if(anchor_one_midpoints[i] > ((minBase + maxBase)/2)){
-            grid.curve(x1=anchor_one_midpoints[i], y1=y.start, x2=maxBase, y2=0.95, curvature = -1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
+            grid.curve(x1=anchor_one_midpoints[i], y1=y.start, x2=maxBase, y2=0.95, 
+                       curvature = -1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
           }else{
-            grid.curve(x1=anchor_one_midpoints[i], y1=y.start, x2=minBase, y2=0.95, curvature = 1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
+            grid.curve(x1=anchor_one_midpoints[i], y1=y.start, x2=minBase, y2=0.95, 
+                       curvature = 1, default.units="native", gp=gpar(col=cols[i],lwd=lwds[i]))
           }
         }
       }
@@ -234,9 +244,20 @@ setMethod("drawGD", signature("InteractionTrack"), function(GdObject, minBase, m
     if(displayPars(GdObject, "interaction.strength")=="height" && displayPars(GdObject, "lwd.interaction") == "counts"){
       curve.heights = anchor.height + (( 1 - anchor.height ) * interactionCounts(GdObject@giobject)/max(interactionCounts(GdObject@giobject)))
     }else if(displayPars(GdObject, "interaction.strength")=="height" && displayPars(GdObject, "lwd.interaction") == "fdr"){
-      curve.heights = anchor.height + ((1 - anchor.height ) * (1.0-GdObject@giobject$fdr))
+      if(is.infinite(log10(GdObject@giobject$fdr))){
+        fdr = 2.2e-16 # really the smallest value that R can represent
+      }else{
+        fdr = 1 - GdObject@giobject$fdr
+      }
+      curve.heights = anchor.height + (1-anchor_height -log10(fdr)) # TODO
+      #curve.heights = anchor.height + ((1 - anchor.height ) * (1.0-GdObject@giobject$fdr))
     }else if(displayPars(GdObject, "interaction.strength")=="height" && displayPars(GdObject, "lwd.interaction") == "p.value"){
-      curve.heights = anchor.height +  ((1 - anchor.height ) * (1.0-GdObject@giobject$p.value))
+      if(is.infinite(log10(GdObject@giobject$p.value))){
+        p.value = 2.2e-16 # really the smallest value that R can represent
+      }else{
+        p.value = 1 - GdObject@giobject$p.value
+      }
+      curve.heights = anchor.height + (1-anchor_height -log10(p.value)) # TODO
     }else{
       curve.heights = rep(1, length(anchor_one_chr))
     }
@@ -249,7 +270,10 @@ setMethod("drawGD", signature("InteractionTrack"), function(GdObject, minBase, m
         xs = c(xs, c(anchor_one_midpoints[i], mdpt, anchor_two_midpoints[i]))
         ys = c(ys, c(y.start, curve.heights[i], y.start))
       } 
-      grid.xspline(xs, ys, id.lengths=rep(3, length(outside.indexes)), shape=-1, default.units="native", gp=gpar(col=cols[outside.indexes], lwd=lwds[outside.indexes]))
+      grid.xspline(xs, ys, id.lengths=rep(3, length(outside.indexes)), 
+                   shape=-1, default.units="native", 
+                   gp=gpar(col=cols[outside.indexes], 
+                           lwd=lwds[outside.indexes]))
     }
     if(length(inside.indexes)>0){
       xs = c()
@@ -259,19 +283,73 @@ setMethod("drawGD", signature("InteractionTrack"), function(GdObject, minBase, m
         xs = c(xs, c(anchor_one_midpoints[i], mdpt, anchor_two_midpoints[i]))
         ys = c(ys,  c(y.start, curve.heights[i], y.start))
       }
-      grid.xspline(xs, ys, id.lengths=rep(3, length(inside.indexes)), shape=-1, default.units="native", gp=gpar(col=cols[inside.indexes], lwd=lwds[inside.indexes]))
+      grid.xspline(xs, ys, id.lengths=rep(3, length(inside.indexes)), 
+                   shape=-1, default.units="native", 
+                   gp=gpar(col=cols[inside.indexes], 
+                           lwd=lwds[inside.indexes]))
     }  
     if(plot.anchors & length(GdObject@giobject) > 0){
+      
       anchors = unique(c(anchorOne(GdObject@giobject), anchorTwo(GdObject@giobject)))
+      
+      col.anchors.fill = rep(col.anchors.fill, length(anchors))
+      if(length(col.anchors.fill.node.class) > 0 & "node.class" %in% names(elementMetadata(anchors))){
+        col.anchors.fill[ anchors$node.class %in% names(col.anchors.fill.node.class)] = col.anchors.fill.node.class[ anchors$node.class ]
+      } 
+        
+      col.anchors.line = rep(col.anchors.line, length(anchors))
+      if(length(col.anchors.line.node.class) > 0 & "node.class" %in% names(elementMetadata(anchors))){
+        col.anchors.line[ anchors$node.class %in% names(col.anchors.line.node.class)] = col.anchors.line.node.class[ anchors$node.class ]
+      } 
+      
       xs = c()
       ys = c()
       for(i in 1:length(anchors)){
         xs = c(xs, c(start(anchors[i]), end(anchors[i]), end(anchors[i]), start(anchors[i])))
         ys = c(ys, c(0, 0, anchor.height, anchor.height))
       }
-      grid.polygon(xs, ys, id.lengths=rep(4, length(anchors)), default.units="native", gp=gpar(col= "black", fill= col.anchors))
+      grid.polygon(xs, ys, id.lengths=rep(4, length(anchors)), 
+                   default.units="native", 
+                   gp=gpar(col=col.anchors.line, 
+                           fill= col.anchors.fill))
     }
   }
   popViewport(1)
   return(invisible(GdObject))
 })
+
+
+
+#' The default display parameters for a track object class can be queries using the availableDisplayPars function.
+#' 
+#' @param class A valid track object class name, or the object itself, in which case the class is derived directly 
+#' from it. 
+#'
+#' This function provides the same functionality as \code{Gviz::InteractionTrack} and allows the user to display 
+#' the default display parameters for the class. If the class of the track is not an \code{InteractionTrack} then 
+#' the function calls the availableDisplayPars method in Gviz.
+#' 
+#' @return returns a list of the default display parameters.
+#' 
+#' @importFrom Gviz availableDisplayPars InferredDisplayPars
+#' @export
+availableDisplayPars = function(class){
+  if(class=="InteractionTrack"){
+    parents <- names(getClassDef(class)@contains)
+    
+    pars =try(sapply(c(parents, "InteractionTrack"), function(x) as(getClassDef(x)@prototype@dp, "list"), simplify=FALSE), silent=TRUE)
+    finalPars <- list()
+    inherited <- list()
+    for(p in names(pars))
+    {
+      finalPars[names(pars[[p]])] <- pars[[p]]
+      inherited[names(pars[[p]])] <- p
+    }
+    finalPars <- finalPars[order(names(finalPars))]
+    inherited <- inherited[order(names(inherited))]
+    return(new("InferredDisplayPars", name=class, inheritance=unlist(inherited), finalPars)) 
+  }else{
+    Gviz::availableDisplayPars(class)
+  }
+}
+
