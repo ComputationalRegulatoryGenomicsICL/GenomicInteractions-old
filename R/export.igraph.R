c52b57f5e343a5498fd0a464669d631f5282b0b7
Liz Ing-Simmons <liz.ingsimmons@gmail.com>
2016-02-24 12:59:59 +0000

    updated export.igraph

---
+++ R/export.igraph.R
@@ -0,0 +1,34 @@
+
+#' Export interactions to an igraph object.
+#'
+#' Exports a GenomicInteractions object to graph.data.frame for use by igraph package. This uses unique anchors
+#' as nodes and generates edges between them. For the resulting graph to be easily interpretable, anchors
+#' should be non-overlapping. This should already be the case for HiC data (either binned or restriction
+#' fragments), however ChIA-PET data can contain overlapping anchors, which may need to be reduced to
+#' non-overlapping regions before graph export.
+#' @param GIObject A GenomicInteractions object.
+#'
+#' @return a graph.data.frame representation of the GenomicInteractions object
+#' @importFrom igraph graph.data.frame
+#'
+#' @export
+#' @docType methods
+#' @rdname export.igraph
+#' @export
+setGeneric("export.igraph",function(GIObject){standardGeneric ("export.igraph")})
+#' @rdname export.igraph
+#' @export
+setMethod("export.igraph", "GenomicInteractions", function(GIObject){
+  
+  if(!is.null(names(regions(GIObject)))){
+    nodes <- names(regions(GIObject))
+  } else {
+    nodes <- as.character(regions(GIObject))
+  }
+  nodes <- data.frame(name = nodes, mcols(regions(GIObject)))
+  
+  edges <- data.frame(from = nodes$name[GIObject@anchor1],
+                      to = nodes$name[GIObject@anchor2])
+  
+  return(graph_from_data_frame(edges, directed=FALSE, vertices = nodes))
+})
