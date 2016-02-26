f916465c608449251fe94182cb58178786df6723
Liz Ing-Simmons <liz.ingsimmons@gmail.com>
2016-02-26 21:11:39 +0000

    removed .Rd file for unexported function

--- R/library.R
+++ R/library.R
@@ -1,11 +1,11 @@
-#' Capitalize first letter of string
-#'
-#' This function will capitalize the first letter of each string in
-#' a character vector, and lowercase following letters.
-#'
-#' @param x A character vector
-#'
-#' @return a string with the first letter capitalised
+# Capitalize first letter of string
+#
+# This function will capitalize the first letter of each string in
+# a character vector, and lowercase following letters.
+#
+# @param x A character vector
+#
+# @return a string with the first letter capitalised
 capitalize = function(x) {
     paste0(toupper(substr(x, 1, 1)), tolower(substring(x, 2)))
 }
